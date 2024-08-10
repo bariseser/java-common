f1f72c17023f30758959707e16b2bd6e85433e8d
b.eser <baris.eser@dataroid.com>
2024-08-10 14:09:00 +0300

    first commit

--- lib/src/test/java/org/example/LibraryTest.java
+++ lib/src/test/java/org/example/LibraryTest.java
@@ -3,12 +3,6 @@
  */
 package org.example;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class LibraryTest {
-    @Test public void someLibraryMethodReturnsTrue() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+
 }
